8e3302191783f6b21f1ecb2c1b477c15cf5a0b0c
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-14 16:21:56 +0800

    Build FeedVC and GroupsVC in interface builder

--- BreakPoint/BreakPoint/Controller/FeedVC.swift
+++ BreakPoint/BreakPoint/Controller/FeedVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  FeedVC.swift
 //  BreakPoint
 //
 //  Created by Jeffrey Umandap on 14/01/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- BreakPoint/BreakPoint/Controller/GoupsVC.swift
+++ BreakPoint/BreakPoint/Controller/GoupsVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  GoupsVC.swift
 //  BreakPoint
 //
 //  Created by Jeffrey Umandap on 14/01/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class GoupsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
