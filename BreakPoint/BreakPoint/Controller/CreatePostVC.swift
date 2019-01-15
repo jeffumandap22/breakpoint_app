16b9e2660cf944761beba1f154689dcc883243c5
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-15 11:36:53 +0800

    Created CreatePostVC and uploading Post to FireBase DB

---
+++ BreakPoint/BreakPoint/Controller/CreatePostVC.swift
@@ -0,0 +1,49 @@
+//
+//  CreatePostVC.swift
+//  BreakPoint
+//
+//  Created by Jeffrey Umandap on 15/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+
+    
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+    }
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        if textView.text != nil && textView.text != "Say something here..." {
+            sendButton.isEnabled = false
+            DataService.instance.uploadPost(withMessage: textView.text, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: nil, sendComplete: { (isComplete) in
+                if isComplete {
+                    self.sendButton.isEnabled = true
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    self.sendButton.isEnabled = true
+                    print("There was an error!")
+                }
+            })
+        }
+    }
+    
+}
+
+extension CreatePostVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+    }
+}
