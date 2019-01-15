6ad1488e34de0f7ad04e356c47abfbda405c1b49
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-15 10:57:30 +0800

    Build MeVC and added to UITabBarController

---
+++ BreakPoint/BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Jeffrey Umandap on 15/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+    }
+    
+}
