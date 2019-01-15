24fd2cd8a69071e2ff7449361244b7b47452fee4
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-15 17:12:23 +0800

    Created CreateGroupVC and connected IBOutlets and Actions

---
+++ BreakPoint/BreakPoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,32 @@
+//
+//  CreateGroupsVC.swift
+//  BreakPoint
+//
+//  Created by Jeffrey Umandap on 15/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextfield: InsetTextField!
+    @IBOutlet weak var descriptionTextfield: InsetTextField!
+    @IBOutlet weak var emailsearchTextfield: InsetTextField!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var groupMemberLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+    }
+    @IBAction func closeButton(_ sender: Any) {
+    }
+    
+    
+}
