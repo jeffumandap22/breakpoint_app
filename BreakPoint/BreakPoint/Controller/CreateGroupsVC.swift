d37e4154525f587d9a372f0d204806e7faca8f6a
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-15 17:53:48 +0800

    Created UserCell

--- BreakPoint/BreakPoint/Controller/CreateGroupsVC.swift
+++ BreakPoint/BreakPoint/Controller/CreateGroupsVC.swift
@@ -15,18 +15,37 @@ class CreateGroupsVC: UIViewController {
     @IBOutlet weak var emailsearchTextfield: InsetTextField!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var groupMemberLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
     }
     @IBAction func closeButton(_ sender: Any) {
     }
+
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "marty@test.com", isSelected: true)
+        
+        return cell
+    }
 }
---
+++ BreakPoint/BreakPoint/View/UserCell.swift
@@ -0,0 +1,39 @@
+//
+//  UserCell.swift
+//  BreakPoint
+//
+//  Created by Jeffrey Umandap on 15/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = image
+        self.emailLabel.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
